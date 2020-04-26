6c4d031d63bfd65b7ca94017cae2e38f5a4d50ec
Jurijs Salijevs <jurijs.salijevs@gmail.com>
2020-04-26 14:37:42 +0300

    budget task v1

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict'
 
-let a = 5;
+let money = prompt("Your budget?"); 
+let time = prompt('Your birthdate?');
+let objective = prompt('What do you want to buy in this month?');
+let budget = prompt('How much does it cost?');
+let optionalExpenses = "";
+let income = [];
+let savings = false;
 
-console.log(a)
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        objective,
+        budget
+    },
+    optionalExpenses: "",
+    income: "",
+    savings: "",
+}
+
+console.log(appData.budget);
+console.log(appData.budget);
+console.log(appData.expenses);
+console.log(appData.expenses);
+alert(appData.budget)
